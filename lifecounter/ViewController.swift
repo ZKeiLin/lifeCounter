31401553e2a25b19c41ff56bcdb74ca8f93d3b76
ZKeiLin <zlin2016@uw.edu>
2019-01-26 11:57:59 -0800

    Initial Commit

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by Zhiqi Lin on 1/26/19.
+//  Copyright © 2019 Zhiqi Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
